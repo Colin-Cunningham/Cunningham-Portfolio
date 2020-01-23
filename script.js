ae216a153fb996bfa02fe83a0a187a17877427fd
Colin Cunningham <colin.199643@gmail.com>
2020-01-23 14:28:40 -0500

    Used framework of old site to put it all on one conitnus scrolling page after user testing showed no one went to the second page.

--- script.js
+++ script.js
@@ -3,32 +3,32 @@ var resumeSec =$("#resume")
 var socialSec =$("#social")
 
 
-$(resumeSec).click(function(){
-    $("#resumeSwitch").removeClass("hide")
-    $("#aboutSwitch").addClass("hide")
-    $("#socialSwitch").addClass("hide")
-    $(resumeSec).addClass("active")
-    $(aboutSec).removeClass("active")
-    $(socialSec).removeClass("active")
-})
+// $(resumeSec).click(function(){
+//     $("#resumeSwitch").removeClass("hide")
+//     $("#aboutSwitch").addClass("hide")
+//     $("#socialSwitch").addClass("hide")
+//     $(resumeSec).addClass("active")
+//     $(aboutSec).removeClass("active")
+//     $(socialSec).removeClass("active")
+// })
 
-$(aboutSec).click(function(){
-    $("#aboutSwitch").removeClass("hide")
-    $("#resumeSwitch").addClass("hide")
-    $("#socialSwitch").addClass("hide")
-    $(aboutSec).addClass("active")
-    $(resumeSec).removeClass("active")
-    $(socialSec).removeClass("active")
-})
+// $(aboutSec).click(function(){
+//     $("#aboutSwitch").removeClass("hide")
+//     $("#resumeSwitch").addClass("hide")
+//     $("#socialSwitch").addClass("hide")
+//     $(aboutSec).addClass("active")
+//     $(resumeSec).removeClass("active")
+//     $(socialSec).removeClass("active")
+// })
 
-$(socialSec).click(function(){
-    $("#socialSwitch").removeClass("hide")
-    $("#resumeSwitch").addClass("hide")
-    $("#aboutSwitch").addClass("hide")
-    $(socialSec).addClass("active")
-    $(aboutSec).removeClass("active")
-    $(resumeSec).removeClass("active")
-})
+// $(socialSec).click(function(){
+//     $("#socialSwitch").removeClass("hide")
+//     $("#resumeSwitch").addClass("hide")
+//     $("#aboutSwitch").addClass("hide")
+//     $(socialSec).addClass("active")
+//     $(aboutSec).removeClass("active")
+//     $(resumeSec).removeClass("active")
+// })
 
 
 $(function(){
@@ -41,6 +41,3 @@ $(function(){
 });
 
 
-$("#submit").click(function(){
-    alert("Your Message Has been Submitted!")
-})
