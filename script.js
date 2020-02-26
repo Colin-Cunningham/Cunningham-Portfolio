ea1ed39f2f3a3ee91dc210eb433f4136dcd78cf1
Colin Cunningham <colin.199643@gmail.com>
2020-02-26 13:56:50 -0500

    created a projects page, that holds every single project I have done. Will continue to update throughout the week

--- script.js
+++ script.js
@@ -3,34 +3,6 @@ var resumeSec =$("#resume")
 var socialSec =$("#social")
 
 
-// $(resumeSec).click(function(){
-//     $("#resumeSwitch").removeClass("hide")
-//     $("#aboutSwitch").addClass("hide")
-//     $("#socialSwitch").addClass("hide")
-//     $(resumeSec).addClass("active")
-//     $(aboutSec).removeClass("active")
-//     $(socialSec).removeClass("active")
-// })
-
-// $(aboutSec).click(function(){
-//     $("#aboutSwitch").removeClass("hide")
-//     $("#resumeSwitch").addClass("hide")
-//     $("#socialSwitch").addClass("hide")
-//     $(aboutSec).addClass("active")
-//     $(resumeSec).removeClass("active")
-//     $(socialSec).removeClass("active")
-// })
-
-// $(socialSec).click(function(){
-//     $("#socialSwitch").removeClass("hide")
-//     $("#resumeSwitch").addClass("hide")
-//     $("#aboutSwitch").addClass("hide")
-//     $(socialSec).addClass("active")
-//     $(aboutSec).removeClass("active")
-//     $(resumeSec).removeClass("active")
-// })
-
-
 $(function(){
 	$("#modal").click(function(){
 		$(".lay").modal('show');
@@ -41,3 +13,6 @@ $(function(){
 });
 
 
+$('.special.cards .image').dimmer({
+	on: 'hover'
+  });
